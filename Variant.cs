6a1eb36ea04ef75f7fc450b9ad47c946d3494067
DamarArts <46912986+DamarArts@users.noreply.github.com>
2019-02-09 22:13:10 -0500

    Update Variant.cs

--- Variant.cs
+++ Variant.cs
@@ -7,15 +7,10 @@ public class Variant: MonoBehaviour
     Vector3 rotate1 = new Vector3(0, 360, 0);
     Vector3 rotate2 = new Vector3(0, -360, 0);
 
-    Vector3 pointA = new Vector3(-1f, 1.5f, 36f);
-    Vector3 pointB = new Vector3(-16.5f, 1.5f, 36f);
+
     void Update()
     {
         transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
-
-        transform.Rotate(Vector3.Lerp(rotate1, rotate2, Mathf.PingPong(Time.time, 1)));
-
-
     }
 
 }
